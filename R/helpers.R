47d0f4835705c0e6ed4eb590547838f233bedc68
Paul Meinz <paulmeinz@gmail.com>
2018-05-30 10:02:50 -0700

    FIXED A GRAMMAR ERROR

--- R/helpers.R
+++ R/helpers.R
@@ -4,5 +4,5 @@ cohortMessage <- function(year, definition) {
   paste("Displaying data for the <strong>", year, "</strong> fall cohort (",
         definition, " students). ",
         "If you would like to disaggregate or conduct a comparison, select
-        'Yes' in the options below", sep = '')
+        'Yes' in the options below.", sep = '')
 }

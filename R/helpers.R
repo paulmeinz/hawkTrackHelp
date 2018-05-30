f9c3bdacdbe8d14bfe1b230b892032930673c562
Paul Meinz <paulmeinz@gmail.com>
2018-05-30 10:01:26 -0700

    MINOR CHANGE TO MESSAGE

--- R/helpers.R
+++ R/helpers.R
@@ -2,7 +2,7 @@
 
 cohortMessage <- function(year, definition) {
   paste("Displaying data for the <strong>", year, "</strong> fall cohort (",
-        definition, " students) ",
+        definition, " students). ",
         "If you would like to disaggregate or conduct a comparison, select
-        'Yes' in the options below")
+        'Yes' in the options below", sep = '')
 }

48ae21fb49e38b169fda8067745af7cd4eb77778
Paul Meinz <paulmeinz@gmail.com>
2018-05-30 09:58:50 -0700

    ADDED COHORT TYPE TO cohortMessage

--- R/helpers.R
+++ R/helpers.R
@@ -1,7 +1,8 @@
 #' @export
 
-cohortMessage <- function(year) {
-  paste("Displaying data for the <strong>", year, "</strong> fall cohort.",
+cohortMessage <- function(year, definition) {
+  paste("Displaying data for the <strong>", year, "</strong> fall cohort (",
+        definition, " students) ",
         "If you would like to disaggregate or conduct a comparison, select
         'Yes' in the options below")
 }

91a5820e869485961b4812d05c988afa0ba41712
Paul Meinz <paulmeinz@gmail.com>
2018-05-31 13:17:41 -0700

    FIXED Cohort disaggregation function to accomodate zeros

--- R/helpers.R
+++ R/helpers.R
@@ -18,27 +18,58 @@ cohortSelectData <- function(year, definition, demographic, data) {
   # Specify filter column based on cohort selection
   names(temp)[names(temp) == definition] <- 'filt'
 
-  plotSet <- temp %>%
-    filter(!is.na(filt)) %>%
-    group_by_(demographic) %>%
-    summarise(headcount = n()) %>%
-    mutate(percent = headcount/sum(headcount) * 100, headcount,
-           total = sum(headcount))
+  filtTest <- temp %>% filter(!is.na(filt))
+  num <- nrow(filtTest)
+
+  if (num >= 1) {
+    plotSet <- temp %>%
+      filter(!is.na(filt)) %>%
+      group_by_(demographic) %>%
+      summarise(headcount = n()) %>%
+      mutate(percent = headcount/sum(headcount) * 100, headcount,
+             total = sum(headcount)) %>%
+      complete_(demographic)
+  } else {
+    plotSet <- temp %>%
+      mutate(none = as.numeric(is.na(emplid))) %>%
+      group_by_(demographic) %>%
+      summarise(headcount = sum(none)) %>%
+      mutate(percent = headcount/sum(headcount) * 100, headcount,
+             total = sum(headcount)) %>%
+      complete_(demographic)
+  }
+
+
+
+  plotSet[is.na(plotSet)] <- 0
 
   plotSet
 }
 
-makeDemoToolTip <- function(num = 'headcount', den = 'total') {
-  paste(
-    "#!
-    function(key, x, y, e) {
-    return '<p> <strong>' + x + '</strong> </p>' +
-    '<p>' +
-    '<strong>' + y + '% </strong>' +
-    '</p>' +
-    '<p>' +
-    e.point.headcount + ' out of ' + e.point.total + ' students' +
-    '</p>'
+makeDemoToolTip <- function(type = 'bar') {
+  if (type == 'bar') {
+    paste(
+      "#!
+      function(key, x, y, e) {
+      return '<p> <strong>' + x + '</strong> </p>' +
+      '<p>' +
+      '<strong>' + y + '% </strong>' +
+      '</p>' +
+      '<p>' +
+      e.point.headcount + ' out of ' + e.point.total + ' students' +
+      '</p>'
+      } !#"
+    )
+  } else {
+      "#!
+      function(key, y, e, graph) {
+      return '<p> <strong>' + key + '</strong> </p>' +
+      '<p>' +
+      '<strong>' + y + '% </strong>' +
+      '</p>' +
+      '<p>' +
+      e.point.headcount + ' out of ' + e.point.total + ' students' +
+      '</p>'
     } !#"
-  )
+  }
 }
